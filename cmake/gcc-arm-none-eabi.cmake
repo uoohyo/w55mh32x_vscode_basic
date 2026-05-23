6122f8a692d6e2e0352ae357913b38295942619d
uoohyo <juwon412@naver.com>
2026-05-23 23:09:28 +0900

    🔧 Support ARM_TOOLCHAIN_DIR for Windows toolchain path
    Allow cmake/gcc-arm-none-eabi.cmake to resolve arm-none-eabi-gcc
    from ARM_TOOLCHAIN_DIR cmake/env variable when not on system PATH.
    Add CMakeUserPresets.json to .gitignore for machine-local overrides.

--- cmake/gcc-arm-none-eabi.cmake
+++ cmake/gcc-arm-none-eabi.cmake
@@ -6,7 +6,21 @@ set(CMAKE_SYSTEM_PROCESSOR  arm)
 # Avoid host-link test on a bare-metal compiler.
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-set(TOOLCHAIN_PREFIX arm-none-eabi-)
+# Toolchain binary directory.
+# When arm-none-eabi-gcc is not on PATH, point ARM_TOOLCHAIN_DIR at the
+# directory that contains the compiler executables.
+#   * CMake variable:  -DARM_TOOLCHAIN_DIR="C:/path/to/bin"
+#   * Env variable:    ARM_TOOLCHAIN_DIR=C:/path/to/bin
+#   * CMakeUserPresets.json cacheVariables (recommended on Windows so the
+#     setting is checked into a developer-local file, not the shared repo)
+if(DEFINED ARM_TOOLCHAIN_DIR)
+    set(TOOLCHAIN_PREFIX "${ARM_TOOLCHAIN_DIR}/arm-none-eabi-")
+elseif(DEFINED ENV{ARM_TOOLCHAIN_DIR})
+    set(TOOLCHAIN_PREFIX "$ENV{ARM_TOOLCHAIN_DIR}/arm-none-eabi-")
+else()
+    set(TOOLCHAIN_PREFIX "arm-none-eabi-")
+endif()
+
 if(WIN32)
     set(TOOLCHAIN_EXT ".exe")
 else()
